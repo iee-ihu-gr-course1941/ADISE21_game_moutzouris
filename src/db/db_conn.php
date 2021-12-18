2974e8b3b6e4d5027692e7ed28d4c0acec6694b2
Panagiotis Malliotas <pmalliotas@gmail.com>
2021-12-18 12:27:58 +0200

    v0.2.3

--- src/db/db_conn.php
+++ src/db/db_conn.php
@@ -5,15 +5,8 @@ $username = "root";
 $password = "";
 $db_name = "moutzouris";
 
-$mysqli = new mysqli($host, $user, $pass, $db, null, '/home/staff/it154486/mysql/run/mysql.sock');
+$mysqli = new mysqli($host, $user, $pass, $db, null, '/home/student/it/2015/it154486/mysql/run/mysql.sock');
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" .
 		$mysqli->connect_errno . ") " . $mysqli->connect_error;
 } ?>
-
-
-// $conn = mysqli_connect($sname, $username, $password, $db_name);
-
-// if (!$conn) {
-// echo "Connection failed!";
-// }
