e05b02215068fcaff9babde87fa037d335ab6286
MILTIADIS PAPAMAVROUDIS <papamavroudis@yahoo.gr>
2021-12-20 20:23:25 +0200

    v.0.5.7 - and register

--- src/auth/login.php
+++ src/auth/login.php
@@ -15,6 +15,11 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 	$username = validate($_POST['username']);
 	$pass = validate($_POST['password']);
 
+	
+
+
+
+
 	if (empty($username)) {
 		header("Location: ../index.php?error=Το όνομα χρήστη είναι απαραίτητο");
 		exit();
@@ -22,6 +27,13 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 		header("Location: ../index.php?error=Ο κωδικός είναι απαραίτητος");
 		exit();
 	} else {
+		//EDW GINETAI TO REGISTER SE PERIPTOSI POY DEN YPARXEI TO USERNAME
+		$sql_u="SELECT * FROM users WHERE username='$username'";
+		$res_u=mysqli_query($conn,$sql_u) or die(mysqli_error($conn));
+		if(mysqli_num_rows($res_u) == 0){			
+			$sql = "INSERT INTO users VALUES (default,'$username','$pass')";
+			$result=mysqli_query($conn,$sql) or die (mysqli_error($conn));	
+		}
 		$sql = "SELECT * FROM users WHERE username='$username' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
